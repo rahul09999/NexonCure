76c9fca12c1ce320051fb8ef98100f713e5a8757
mohammedanafmaster <mohammedanafm@gmail.com>
2024-08-05 19:55:44 +0530

    Home page modified

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
  return(
   <div>
-    <h1 className="text-white text-3xl underline">Home</h1>
-    <Button>Click Me</Button>
+    <h1 className="font-bold text-9xl flex h-screen items-center justify-center">Hello, World</h1>
   </div>
  )
 }
