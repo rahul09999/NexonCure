94868287baeda349fd652aa4a4782ccb3868eb65
mohammedanafmaster <mohammedanafm@gmail.com>
2024-08-08 12:48:47 +0530

    Home page initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,40 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello, World</h1>
-      <Button>Click Me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            className="mb-12 h-10 w-fit"
+            alt="logo"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <address className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2024 NexonCure
+            </address>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="hero banner"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
