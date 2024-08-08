f7034ef0608119f70f3e1a6c6de2900f41cdaaa6
Nishrajmalhi <2102030400154@silveroakuni.ac.in>
2024-08-08 15:51:39 +0530

    Setting up the repo

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
  return(
   <div>
     <h1 className="font-bold text-9xl flex h-screen items-center justify-center">Hello, World</h1>
+    <p className="flex justify-center items-center">Lorem ipsum dolor sit amet</p>
   </div>
  )
 }
